c25a8df6a76fe4343b24da18c0baf69e3dcb17d6
TheLazron <teddybearlaughs@gmail.com>
2025-05-15 23:24:15 +0530

    fix: create-item name check for dot enteries

--- src/internal/function.go
+++ src/internal/function.go
@@ -212,6 +212,16 @@ func removeElementByValue(slice []string, value string) []string {
 	return newSlice
 }
 
+func checkInvalidDestination(name string) bool {
+	switch {
+	case name == ".", name == "..":
+		return false
+	case strings.HasSuffix(name, "/."), strings.HasSuffix(name, "/.."):
+		return false
+	}
+	return true
+}
+
 func renameIfDuplicate(destination string) (string, error) {
 	info, err := os.Stat(destination)
 	if os.IsNotExist(err) {
